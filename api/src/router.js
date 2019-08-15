ee8958690171e28cd4f026022150ca0b3d205808
Assisneto <netodamasceno12@gmail.com>
2019-08-15 15:22:50 -0300

    first message with Kafka

--- api/src/router.js
+++ api/src/router.js
@@ -2,8 +2,19 @@ import { Router } from "express";
 
 const routes = new Router();
 
-routes.post("/certifications", (req, res) => {
+routes.post("/certifications", async (req, res) => {
   //Micro-service call
+
+  const message = {
+    user: { id: 1, name: "Assisneto Damasceno" },
+    course: "Kafka with Node",
+    grade: 5
+  };
+
+  await req.producer.send({
+    topic: "issue-certificate",
+    messages: [{ value: JSON.stringify(message) }]
+  });
   return res.json({ ok: true });
 });
 
--- api/src/server.js
+++ api/src/server.js
@@ -1,19 +1,30 @@
 import express from "express";
 import { Kafka } from "kafkajs";
+
 import routes from "./router";
 
 const app = express();
 
 const kafka = new Kafka({
   clientId: "api",
-  brokers: ["kafka:9092"]
+  brokers: ["localhost:9092"]
+});
+
+const producer = kafka.producer();
+
+app.use((req, res, next) => {
+  req.producer = producer;
+
+  return next();
 });
 
 app.use(routes);
 
-const producer = kafka.producer();
-
 async function run() {
   await producer.connect();
-  app.listen(3000);
+  app.listen(3003, () => {
+    console.log("Server on");
+  });
 }
+
+run().catch(console.error);
